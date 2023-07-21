61e71617b447d06300089eb434c37a662ac696ac
shishkin015 <138977966+shishkin015@users.noreply.github.com>
2023-07-21 15:07:17 +0300

    Update main.py
    fix main.py

--- utils/main.py
+++ utils/main.py
@@ -1,6 +1,16 @@
+from utils.arrs import *
+import os.path
 # <дата перевода> <описание перевода>
 # <откуда> -> <куда>
 # <сумма перевода> <валюта>
 
-def receive_transactions():
-    for
+if __name__ == '__main__':
+    # получаем данные из operations.json
+    data_json = transforms_operations(os.path.join("../operations/operations.json"))
+    # Фильтрует только выполненные транзакции и сортируетпо дате исполнения
+    data_json = returns_executed(data_json)
+    # Обрабатывает делали операций
+    data_json = production_elements_operations(data_json)
+    # Выводит последние 5 транзакций
+    for transaction in data_json[:4]:
+        print(transaction.get_description_operation())
